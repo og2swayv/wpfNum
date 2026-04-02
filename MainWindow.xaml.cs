f272d81f5936bc5c988b2d602f5144e8be43ff53
og2swayv <Chukjora@gmail.com>
2026-04-03 01:48:37 +0300

    Создан базовый интерфейс калькулятора

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,28 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace WpfApp3
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private bool TryGetNumbers(out double a, out double b)
+        {
+            bool firstOk = double.TryParse(FirstNumberTextBox.Text, out a);
+            bool secondOk = double.TryParse(SecondNumberTextBox.Text, out b);
+
+            if (!firstOk || !secondOk)
+            {
+                ResultTextBlock.Text = "Введите корректные числа";
+                return false;
+            }
+
+            return true;
         }
     }
 }
